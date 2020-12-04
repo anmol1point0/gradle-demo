388b3a851cc1cc6081c6f173b234f6bf374c118e
Anmol Mittal <anmol15554@gmail.com>
2020-12-04 17:21:49 +0530

    Milestone 4 complete

--- src/main/java/com/gradleDemo/MyApplication.java
+++ src/main/java/com/gradleDemo/MyApplication.java
@@ -1,7 +1,26 @@
 package com.gradleDemo;
 
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
+
 public class MyApplication {
+
+    public JsonNode getData() throws IOException {
+        ObjectMapper obj = new ObjectMapper();
+        HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
+        HttpRequest request = requestFactory
+                .buildGetRequest(new GenericUrl("https://jsonplaceholder.typicode.com/todos/1"));
+        String response = request.execute().parseAsString();
+        return obj.readValue(response, JsonNode.class);
+    }
+
     public static void main(String args[]) {
-        System.out.println("Heyy!!");
+        // System.out.println("Heyy!!");
     }
 }
